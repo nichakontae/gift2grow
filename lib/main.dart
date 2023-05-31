dd4ac269121514fbf093dfb5c7abf4ec37653519
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-06-01 02:01:30 +0700

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:gift2grow/screen/login.dart';
 import 'package:gift2grow/widgets/theme_button.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const Gift2Grow());
 }
 
