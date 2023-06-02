566da47eb4414ee559eaced4ab7f4b538480bf71
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-06-02 17:20:21 +0700

    [UPDATE] new style

--- lib/screen/verify_email.dart
+++ lib/screen/verify_email.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gift2grow/screen/home.dart';
 import 'package:gift2grow/screen/login.dart';
+import 'package:gift2grow/widgets/background_gradient.dart';
 import 'package:gift2grow/widgets/theme_button.dart';
 
 class VerifyEmailPage extends StatefulWidget {
@@ -64,60 +65,88 @@ class _VerifyEmailPageState extends State<VerifyEmailPage> {
 
   @override
   Widget build(BuildContext context) => isEmailVerified
-      ? HomePage()
-      : Scaffold(
-          body: Container(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "assets/logo/mail.png",
-                        scale: 2,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        "We've send an email to ${widget!.email} to verify your email address and activate your account.",
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      CustomButton(
-                          color: "primary",
-                          text: "Resend Email",
-                          onTap: () {
-                            canResendEmail ? sendVerificationEmail : null;
-                          }),
-                      ElevatedButton(
-                        style: ButtonStyle(),
-                          onPressed:
-                              canResendEmail ? sendVerificationEmail : null,
-                          child: Text("Resend Email")),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      CustomButton(
-                          color: "secondary",
-                          text: "Cancel",
-                          onTap: () {
-                            FirebaseAuth.instance.signOut();
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const LoginPage()));
-                          })
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        );
+      ? const HomePage()
+      : BackgroundGradient(
+        child:  Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                       Container(
+                         padding: const EdgeInsets.all(25),
+                         decoration: const BoxDecoration(
+                             color: Colors.white,
+                             borderRadius: BorderRadius.all(Radius.circular(25))),
+                         child: Column(
+                           children: [
+                             Image.asset(
+                               "assets/logo/mail.png",
+                               scale: 2,
+                             ),
+                             const SizedBox(
+                               height: 20,
+                             ),
+                             RichText(
+                               textAlign: TextAlign.center,
+                               text: TextSpan(
+                                 style: const TextStyle(
+                                     color: Colors.black, fontFamily: "Poppins"),
+                                 children: [
+                                   const TextSpan(
+                                     text: "We've sent an email to ",
+                                   ),
+                                   TextSpan(
+                                     text: widget!.email,
+                                     style: const TextStyle(
+                                       fontWeight: FontWeight.bold,
+                                     ),
+                                   ),
+                                   const TextSpan(text: " to verify"),
+                                   const TextSpan(
+                                       text:
+                                       " your email address and activate your account.")
+                                 ],
+                               ),
+                             ),
+                             const SizedBox(
+                               height: 40,
+                             ),
+                             Row(
+                               mainAxisAlignment: MainAxisAlignment.spaceAround,
+                               children: [
+
+                                 CustomButton(
+                                     paddingHorizontal:
+                                     const EdgeInsets.symmetric(horizontal: 50),
+                                     color: "secondary",
+                                     text: "Cancel",
+                                     onTap: () {
+                                       FirebaseAuth.instance.signOut();
+                                       Navigator.push(
+                                         context,
+                                         MaterialPageRoute(
+                                             builder: (context) =>
+                                             const LoginPage()),);
+                                     }),
+                                 CustomButton(
+                                   color: "primary",
+                                   text: "Resend Email",
+                                   onTap:
+                                   canResendEmail ? sendVerificationEmail : null,
+                                   paddingHorizontal:
+                                   const EdgeInsets.symmetric(horizontal: 30),
+                                 ),
+                               ],
+                             )
+                           ],
+                         ),
+                       )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+      );
 }
