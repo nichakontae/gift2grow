3d75bdeb2e3ccddde5ababec9e5382ca5f9b7560
Lilyliiin <98150547+Lilyliiin@users.noreply.github.com>
2023-06-04 05:50:05 +0700

    card profile

---
+++ lib/widgets/rank_card_profile.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class RankCardProfile extends StatelessWidget {
+  final int index;
+  const RankCardProfile(this.index, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+      child: Row(
+        children: [
+          Text(
+            "#${index.toString()}",
+            style: TextStyle(fontWeight: FontWeight.w600),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 15, 0),
+            child: Image.asset(
+              'assets/images/GrayCircle.png',
+              scale: 4.7,
+            ),
+          ),
+          Text(
+            "username",
+            style: TextStyle(fontWeight: FontWeight.w500),
+          ),
+          Spacer(),
+          Text(
+            "999",
+            style: TextStyle(fontWeight: FontWeight.w500),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(4, 0, 0, 5),
+            child: Image.asset(
+              'assets/images/PointBox.png',
+              scale: 6.3,
+            ),
+          ),
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.end,
+          //   children: [
+
+          //   ],
+          // )
+        ],
+      ),
+    );
+  }
+}
