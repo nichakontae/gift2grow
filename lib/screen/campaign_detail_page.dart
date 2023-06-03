66c0d9b1fc46db8b78b0e81148a2b61b11eaaa7a
bbbung <kanyapat.mekv@kmutt.ac.th>
2023-06-03 22:01:26 +0700

    Create campaign_detail_page.dart

---
+++ lib/screen/campaign_detail_page.dart
@@ -0,0 +1,294 @@
+// import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:gift2grow/models/campaign_detail.dart';
+import 'package:gift2grow/screen/campaign_donate_page.dart';
+import 'package:gift2grow/utilities/caller.dart';
+import 'package:gift2grow/widgets/theme_button.dart';
+import 'package:gift2grow/widgets/unordered_list.dart';
+
+import '../widgets/description.dart';
+import 'full_screen_image.dart';
+
+class CampaignDetailPage extends StatefulWidget {
+  final int campaignId;
+  const CampaignDetailPage({super.key, required this.campaignId});
+
+  @override
+  State<CampaignDetailPage> createState() => _CampaignDetailPageState();
+}
+
+class _CampaignDetailPageState extends State<CampaignDetailPage> {
+  CampaignDetail? campaign;
+// /campaign/getCampaignDetail?campaignId=
+  Future<void> getCampaignDetail() async {
+    try {
+      final response = await Caller.dio.get(
+        '/campaign/getCampaignDetail?campaignId=${widget.campaignId}',
+      );
+      // print(response.data);
+      setState(() {
+        campaign = CampaignDetail(
+          campaignId: response.data["campaignId"],
+          coverImage: response.data["cover_image"],
+          topic: response.data["topic"],
+          location: response.data["location"],
+          schoolName: response.data["school_name"],
+          description: response.data["description"],
+          isComplete: response.data["is_completed"],
+          telContact: response.data["tel_contact"],
+          completedAmount: response.data["completed_amount"],
+          createdAt: response.data["created_at"],
+          wantLists: List<String>.from(response.data["want_lists"]),
+          campaignImages: List<String>.from(response.data["campaign_images"]),
+        );
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+        print('error ja');
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCampaignDetail();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _showFullScreenImage(int currentIndex) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FullScreenImageScreen(
+          imageUrls: campaign!.campaignImages,
+          currentIndex: currentIndex,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (campaign == null) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [CircularProgressIndicator()],
+      );
+    }
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 350,
+                width: 350,
+                child: Stack(
+                  children: [
+                    Card(
+                      color: Colors.deepPurpleAccent[100],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      elevation: 8,
+                      child: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                    Positioned.fill(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30.0),
+                        child: SizedBox(
+                          child: Image.network(
+                            campaign!.coverImage,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 20.0,
+                      left: 20.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(122, 150, 150, 150),
+                          borderRadius: BorderRadius.circular(21.0),
+                        ),
+                        child: IconButton(
+                          color: Colors.white,
+                          icon: const Icon(Icons.arrow_back),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 24, 8, 0),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(campaign!.topic,
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                              color: const Color.fromRGBO(148, 104, 172, 1),
+                            )),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 8),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.school,
+                                size: 25,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                campaign!.schoolName,
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 0, bottom: 16),
+                          child: DescriptionTextWidget(
+                            text: campaign!.description,
+                          ),
+                        ),
+                      ])),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Expanded(
+                      child: Text(
+                        textAlign: TextAlign.start,
+                        'Gallery',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  child: GridView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 200,
+                            // childAspectRatio: 3 / 2,
+                            crossAxisSpacing: 0,
+                            mainAxisSpacing: 0),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: campaign!.campaignImages.length,
+                    itemBuilder: (BuildContext context, int j) {
+                      return GestureDetector(
+                        onTap: () => _showFullScreenImage(j),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12.0),
+                            child: Image.network(
+                              campaign!.campaignImages[j],
+                              fit: BoxFit.cover,
+                              width: 100,
+                              height: 100,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Wanted Lists',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 0, 0, 16),
+                    child: UnorderedList(campaign!.wantLists),
+                  ),
+                ],
+              ),
+              const Divider(
+                thickness: 3,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 16, 0, 32),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(width: 10.0),
+                    Icon(
+                      Icons.phone,
+                      size: 30,
+                    ),
+                    SizedBox(width: 15.0),
+                    Text(
+                      'Tel. ' + campaign!.telContact,
+                      style: const TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+              CustomButton(
+                  color: "primary",
+                  text: "Donate now",
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CampaignDonatePage(
+                                  campaignId: campaign!.campaignId,
+                                  topic: campaign!.topic,
+                                  schoolName: campaign!.schoolName,
+                                  location: campaign!.location,
+                                  coverImage: campaign!.coverImage,
+                                )));
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
