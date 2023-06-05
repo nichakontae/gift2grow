ed310ae26e276f465e210e23dd7f1ba6ccbfbd92
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-06-05 23:49:03 +0700

    [UPDATE]finish save image to gallery

--- lib/widgets/rank_widget/preview_share.dart
+++ lib/widgets/rank_widget/preview_share.dart
@@ -1,10 +1,14 @@
 // ignore_for_file: camel_case_types
 
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:gift2grow/models/rank/user_profile_for_share.dart';
 import 'package:gift2grow/widgets/rank_widget/preview_share_icon.dart';
 import 'package:gift2grow/widgets/rank_widget/profile_for_top_three/profile_frame1st.dart';
 import 'package:gift2grow/widgets/rank_widget/share_template/first_rank.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:screenshot/screenshot.dart';
 
 class PreviewShare extends StatefulWidget {
@@ -16,80 +20,98 @@ class PreviewShare extends StatefulWidget {
 }
 
 class _PreviewShareState extends State<PreviewShare> {
-  final controller = ScreenshotController();
+  final ScreenshotController controller = ScreenshotController();
+
+  Future<String> saveImage(Uint8List bytes) async {
+    await [Permission.storage].request();
+    final time = DateTime.now()
+        .toIso8601String()
+        .replaceAll('.', '-')
+        .replaceAll(':', '-');
+    final name = 'screenshot_$time';
+    final result = await ImageGallerySaver.saveImage(bytes, name: name);
+    return result['filePath'];
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        FirstRank(profile: widget.profile),
-        Positioned(
-          top: 70,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(15, 0, 103, 0),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.of(context).pop(); // Perform the back navigation
-              },
-              child: Image.asset(
-                'assets/icon/cancel.png',
-                scale: 14,
+    return MediaQuery(
+      data: MediaQueryData(),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            FirstRank(profile: widget.profile),
+            Positioned(
+              top: 70,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(15, 0, 103, 0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop(); // Perform the back navigation
+                  },
+                  child: Image.asset(
+                    'assets/icon/cancel.png',
+                    scale: 14,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        Positioned(
-          bottom: 15,
-          right: 20,
-          child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                  //color: Colors.black,
-                  color: Colors.black.withOpacity(0.25), // Shadow color
-                  blurRadius: 1, // Spread radius
-                  offset: const Offset(1, 2), // Offset in x and y directions
+            Positioned(
+              bottom: 15,
+              right: 20,
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      //color: Colors.black,
+                      color: Colors.black.withOpacity(0.25), // Shadow color
+                      blurRadius: 1, // Spread radius
+                      offset: const Offset(1, 2), // Offset in x and y directions
+                    ),
+                  ],
                 ),
-              ],
+                child: GestureDetector(
+                  onTap: () async {
+                    final image = await controller
+                        .captureFromWidget(FirstRank(profile: widget.profile),pixelRatio: MediaQuery.of(context).devicePixelRatio);
+                    if (image == null) return;
+                    await saveImage(image);
+                  },
+                  child: Image.asset(
+                    'assets/icon/download.png',
+                    scale: 12,
+                  ),
+                ),
+              ),
             ),
-            child: GestureDetector(
-              onTap: () async {
-                final image = await controller
-                    .captureFromWidget(FirstRank(profile: widget.profile));
-              },
+            Positioned(
+              bottom: 15,
+              left: 20,
               child: Image.asset(
-                'assets/icon/download.png',
+                'assets/icon/facebook.png',
                 scale: 12,
               ),
             ),
-          ),
+            Positioned(
+              bottom: 14,
+              left: 75,
+              child: Image.asset(
+                'assets/icon/twitter.png',
+                scale: 11.9,
+              ),
+            ),
+            Positioned(
+              bottom: 13.5,
+              left: 130,
+              child: Image.asset(
+                'assets/icon/instagram.png',
+                scale: 11.5,
+              ),
+            ),
+          ],
         ),
-        Positioned(
-          bottom: 15,
-          left: 20,
-          child: Image.asset(
-            'assets/icon/facebook.png',
-            scale: 12,
-          ),
-        ),
-        Positioned(
-          bottom: 14,
-          left: 75,
-          child: Image.asset(
-            'assets/icon/twitter.png',
-            scale: 11.9,
-          ),
-        ),
-        Positioned(
-          bottom: 13.5,
-          left: 130,
-          child: Image.asset(
-            'assets/icon/instagram.png',
-            scale: 11.5,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
--- lib/widgets/rank_widget/share_template/first_rank.dart
+++ lib/widgets/rank_widget/share_template/first_rank.dart
@@ -9,136 +9,144 @@ class FirstRank extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF9569AC),
-                Color(0xFFFFFAEF),
-              ],
-              stops: [0, 0.74],
-            )),
-        child: Column(
-          children: [
-            const Padding(padding: EdgeInsets.fromLTRB(0, 74, 0, 0)),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  "Ranking",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500),
-                ),
-                const Padding(padding: EdgeInsets.all(2)),
-                const Text(
-                  "You are in #1st",
-                  style: TextStyle(
-                      fontSize: 29,
-                      shadows: [
-                        Shadow(
-                          color: Color.fromARGB(255, 100, 98, 98),
-                          offset: Offset(0, 2),
-                          blurRadius: 9,
-                        ),
-                      ],
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(8, 80, 8, 85),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Transform.scale(
-                        scale: 2.2, // Adjust the scale factor as needed
-                        child: Profile1st(profile: profile,),
-                      )
-                    ],
+    return MediaQuery(
+      data: MediaQueryData(),
+      child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF9569AC),
+              Color(0xFFFFFAEF),
+            ],
+            stops: [0, 0.74],
+          )),
+          child: Column(
+            children: [
+              const Padding(padding: EdgeInsets.fromLTRB(0, 74, 0, 0)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Ranking",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500),
                   ),
-                ),
-                Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
+                  const Padding(padding: EdgeInsets.all(2)),
+                  const Text(
+                    "You are in #1st",
+                    style: TextStyle(
+                        fontSize: 29,
+                        shadows: [
+                          Shadow(
+                            color: Color.fromARGB(255, 100, 98, 98),
+                            offset: Offset(0, 2),
+                            blurRadius: 9,
+                          ),
+                        ],
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 20, 10, 30),
-                    child: Column(
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 80, 8, 85),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(profile.username,
-                            style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                                color: Color(0xFF9468AC))),
-                        Text('${profile.tamboonPoint} TAMBOON',
-                            style: const TextStyle(
-                                fontWeight: FontWeight.w400,
-                                color: Color(0xFF9468AC))),
-                        const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                              '"You have made a total of 120 donations"',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  color: Color(0xFFFFC107))),
-                        ),
-                        //ทำเช็คว่าอยู่levelไหน
-                        const Text(
-                          'For truly we are all angels',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            color: Color(0xFF9468AC),
-                          ),
-                        ),
-                        const Text(
-                          'temporarily hiding as humans.',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            color: Color(0xFF9468AC),
-                          ),
+                        Transform.scale(
+                          scale: 2.2, // Adjust the scale factor as needed
+                          child: Profile1st(profile: profile),
                         )
                       ],
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.181,
-              child: Stack(
-                children: [
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Image.asset(
-                      'assets/images/Cloud_right.png',
-                      scale: 1,
+                  Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 20, 10, 30),
+                      child: Column(
+                        children: [
+                          Text(profile.username,
+                              style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xFF9468AC))),
+                          Text('${profile.tamboonPoint} TAMBOON',
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xFF9468AC))),
+                          const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                                '"You have made a total of 120 donations"',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    color: Color(0xFFFFC107))),
+                          ),
+                          //ทำเช็คว่าอยู่levelไหน
+                          const Text(
+                            'For truly we are all angels',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFF9468AC),
+                            ),
+                          ),
+                          const Text(
+                            'temporarily hiding as humans.',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFF9468AC),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    child: Image.asset(
-                      'assets/images/Cloud_left.png',
-                      scale: 1,
-                    ),
-                  ),
-                  // const PreviewShareIcon()
                 ],
               ),
-            ),
-          ],
-        ),
+              // SizedBox(
+              //   width: MediaQuery.of(context).size.width,
+              //   height: MediaQuery.of(context).size.height * 0.181,
+              //   child:
+              Expanded(
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: FractionallySizedBox(
+                        widthFactor: 0.5,
+                        child: Image.asset(
+                          'assets/images/Cloud_right.png',
+                          scale: 0.8,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: FractionallySizedBox(
+                        widthFactor: 0.5,
+                        child: Image.asset(
+                          'assets/images/Cloud_left.png',
+                          scale: 1,
+                        ),
+                      ),
+                    ),
+                    // const PreviewShareIcon()
+                  ],
+                ),
+              ),
+              // ),
+            ],
+          ),
       ),
     );
   }
