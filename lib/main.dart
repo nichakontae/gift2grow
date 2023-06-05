7c75aa3b953ec5b73d547076b56da4d0466e6e08
Ryyyyyy12 <kevalin.sris@kmutt.ac.th>
2023-06-06 01:23:25 +0700

    implement FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,61 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:gift2grow/screen/bottom_navbar.dart';
 import 'package:gift2grow/screen/forgot_password.dart';
 //import 'package:gift2grow/screen/home.dart';
 import 'package:gift2grow/screen/login.dart';
+import 'package:gift2grow/screen/notification.dart';
 import 'package:gift2grow/screen/resgister.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+//background event handler
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
+
+void _firebaseMessagingForegroundHandler(RemoteMessage message) {
+  print('Got a message whilst in the foreground!');
+  print('Message data: ${message.data}');
+
+  if (message.notification != null) {
+    print('Message also contained a notification: ${message.notification}');
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  //background event handler
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  //foreground event handler
+  FirebaseMessaging.onMessage.listen(_firebaseMessagingForegroundHandler);
+
+  //Request permission
+  NotificationSettings settings =
+      await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  //Handle token
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print("FCM token:$fcmToken");
+  //store token to db
+
   runApp(const Gift2Grow());
 }
 
@@ -26,6 +69,15 @@ class Gift2Grow extends StatefulWidget {
 class _Gift2GrowState extends State<Gift2Grow> {
   User? user;
 
+  Future<void> setupInteractedMessage() async {
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+    if (initialMessage != null) {
+      _handleMessageOpenTerminated(initialMessage);
+    }
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessageOpenBackground);
+  }
+
   @override
   void initState() {
     super.initState();
@@ -35,6 +87,18 @@ class _Gift2GrowState extends State<Gift2Grow> {
         user = newUser;
       });
     });
+    setupInteractedMessage();
+  }
+
+  void _handleMessageOpen(RemoteMessage message) {}
+  void _handleMessageOpenTerminated(RemoteMessage message) {
+    print('message from app that was terminated');
+    _handleMessageOpen(message);
+  }
+
+  void _handleMessageOpenBackground(RemoteMessage message) {
+    print('message from app that was in background');
+    _handleMessageOpen(message);
   }
 
   @override
@@ -47,7 +111,7 @@ class _Gift2GrowState extends State<Gift2Grow> {
         '/login': (context) => const LoginPage(),
         '/register': (context) => const RegisterPage(),
         '/forgot-password': (context) => const ForgotPasswordPage(),
-        '/home': (context) => const MyBottomNavbar(),
+        '/home': (context) => const NotificationPage(),
       },
       theme: theme.copyWith(
         colorScheme: theme.colorScheme.copyWith(
--- lib/screen/notification.dart
+++ lib/screen/notification.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/material.dart';
 import 'package:gift2grow/models/user_noti.dart';
 import 'package:intl/intl.dart';
