528ab42db2aa51ff3c79405a1e0c98f9d33ebace
Lilyliiin <98150547+Lilyliiin@users.noreply.github.com>
2023-06-10 13:11:32 +0700

    update rank level frames

--- lib/widgets/rank_widget/three_level_frame/frame_trainee_angel.dart
+++ lib/widgets/rank_widget/three_level_frame/frame_trainee_angel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gift2grow/models/rank/user_profile_for_share.dart';
+import 'package:simple_shadow/simple_shadow.dart';
 
 class FrameTraineeAngel extends StatelessWidget {
   const FrameTraineeAngel({Key? key, required this.profile}) : super(key: key);
@@ -9,10 +10,32 @@ class FrameTraineeAngel extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: 115,
-      height: 94,
+      height: 98,
       child: Stack(
         children: [
           Positioned(
+            top: 2,
+            left: 10,
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.35),
+                    blurRadius: 1,
+                    spreadRadius: -1.75,
+                    offset: const Offset(-0.2, -0.3),
+                  ),
+                ],
+              ),
+              child: Image.asset(
+                'assets/images/GrayCircle.png',
+                scale: 2.68,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 5.5,
             left: 15,
             child: CircleAvatar(
               backgroundImage: profile.profileImage == null ||
@@ -25,19 +48,29 @@ class FrameTraineeAngel extends StatelessWidget {
             ),
           ),
           Positioned(
-            top: 45,
-            left: 52,
-            child: Image.asset(
-              'assets/images/Butterfly.png',
-              scale: 2.7,
+            top: 53.5,
+            left: 54,
+            child: SimpleShadow(
+              opacity: 0.35,
+              offset: const Offset(0, 1),
+              sigma: 0.9,
+              child: Image.asset(
+                'assets/images/Butterfly.png',
+                scale: 2.7,
+              ),
             ),
           ),
           Positioned(
-            top: 32,
-            left: 77,
-            child: Image.asset(
-              'assets/images/Lambent.png',
-              scale: 2.8,
+            top: 40.5,
+            left: 79,
+            child: SimpleShadow(
+              opacity: 0.3,
+              offset: const Offset(0, 1.3),
+              sigma: 0.8,
+              child: Image.asset(
+                'assets/images/Lambent.png',
+                scale: 2.8,
+              ),
             ),
           ),
         ],
