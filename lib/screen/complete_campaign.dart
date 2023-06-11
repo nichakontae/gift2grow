abea9ceda25b0a22e5cd28eff95c63b4c3ee2f3d
Nattawat Teaiwtan <Nao.Nait@gmail.com>
2023-06-11 18:03:30 +0700

    [UPDATE] remove print on completedCampaign page

--- lib/screen/complete_campaign.dart
+++ lib/screen/complete_campaign.dart
@@ -30,7 +30,7 @@ class _CompletedCampaignState extends State<CompletedCampaign> {
 
       CompletedCampaignInfo camapaign =
           CompletedCampaignInfo.fromJson(response.data);
-        print("${camapaign.evidenceImg} + Evidence +  ${campaignInfo?.letterThank}");
+        
       setState(() {
         campaignInfo = camapaign;
         if (campaignInfo!.evidenceImg?.isEmpty == false && (campaignInfo?.letterThank != "" && campaignInfo?.letterThank != null)) {
